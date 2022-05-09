177e849f3e322f47ff94562ef15d80e993afce45
zSnails <erizojuan33@gmail.com>
2022-05-09 02:10:45 -0600

    Bump version

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 $GIT_COMMIT = git rev-parse --short HEAD
 $GIT_USERNAME = git config --get user.name
 $MODULE = "github.com/zSnails/taskr"
-$PROGRAM_VERSION = "2.1.2"
+$PROGRAM_VERSION = "2.2.0"
 
 $env:CGO_ENABLED = 1
 $LD_FLAGS = "-s -w -X '$($MODULE)/internal/command.CommitHash=$($GIT_COMMIT)' -X '$($MODULE)/internal/command.BuildUser=$($GIT_USERNAME)' -X '$($MODULE)/internal/command.Version=$($PROGRAM_VERSION)'"
