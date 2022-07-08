f045ed4c7eab1bdb70d55295c44f18db207c52df
zSnails <erizojuan33@gmail.com>
2022-07-08 09:39:09 -0600

    Remove old build script

--- build.ps1
+++
@@ -1,9 +0,0 @@
-$GIT_COMMIT = git rev-parse --short HEAD
-$GIT_USERNAME = git config --get user.name
-$MODULE = "github.com/zSnails/taskr"
-$PROGRAM_VERSION = "2.2.1"
-
-$env:CGO_ENABLED = 1
-$LD_FLAGS = "-s -w -X '$($MODULE)/internal/command.CommitHash=$($GIT_COMMIT)' -X '$($MODULE)/internal/command.BuildUser=$($GIT_USERNAME)' -X '$($MODULE)/internal/command.Version=$($PROGRAM_VERSION)'"
-$TAGS = "osusergroup,netgo,sqlite_omit_load_extension"
-go build -tags=$TAGS -trimpath -ldflags="$($LD_FLAGS)"
